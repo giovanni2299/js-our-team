68fa9b8c9c3b81e30cfd435002d967f301c58351
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-18 14:17:36 +0100

    ADD: array whit workers

--- js/app.js
+++ js/app.js
@@ -6,11 +6,48 @@ MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite.
 
 MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, 
+Stampare su console, per ogni membro del team, le informazioni di nome,cognome, 
 ruolo e la stringa della foto
 
 MILESTONE 2:
 Stampare le stesse informazioni su DOM semplicemente come stringhe
 */
 
-const workers = [];
+const workers = [
+    {
+        nome: 'Wayne',
+        cognome: 'Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela',
+        cognome: 'Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'  
+    },
+    {
+        nome: 'Walter',
+        cognome: 'Gordon',
+        ruolo: 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg'   
+    },
+    {
+        nome: 'Angela',
+        cognome: 'Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg'  
+    },
+    {
+        nome: 'Scott',
+        cognome: 'Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara',
+        cognome: 'Ramos',
+        ruolo: 'Graphic Designer',
+        foto: ' barbara-ramos-graphic-designer.jpg'
+    }
+];
