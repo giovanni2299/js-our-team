88359c986ea9f437557628c2ebd935165e9d1f8e
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-18 14:02:17 +0100

    add:first structure of the document

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log('ciao mamma')
+
+
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, 
+ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM semplicemente come stringhe
+*/
+
+const workers = []
